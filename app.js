d3f88d45b675df1414386f180ebb70d34e266ce6
Amir Heshm <amirhesham65@gmail.com>
2022-12-25 00:42:09 +0200

    Add basic app shell

---
+++ app.js
@@ -0,0 +1,8 @@
+// Amir Hesham K.
+// 2022
+
+
+var toggleMobileMenu = () => {
+    var mobileMenu = document.getElementById("mobile-menu");
+    mobileMenu.className = (mobileMenu.className == "hidden") ? "block" : "hidden";
+}
